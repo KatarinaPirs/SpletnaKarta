38f5d0105394682404d252f9ca36d9c84ecdd86b
KatarinaPirs <105744192+KatarinaPirs@users.noreply.github.com>
2022-05-17 21:38:40 +0200

    prvo nalaganje

---
+++ data/Cerkniska_transverzalagpxwaypoints_4.js
@@ -0,0 +1,1 @@
+var json_Cerkniska_transverzalagpxwaypoints_4 = {"type":"FeatureCollection","name":"Cerkniska_transverzalagpxwaypoints_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
